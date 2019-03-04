c680118bba33dce5befcec1efdd07be3f70230f7
Gilad Reich <giladreichgr@gmail.com>
2019-03-04 21:53:59 +0100

    Format and solution filters.

--- src/QDirect3D9Widget/ImGui/QDirect3D9Widget.h
+++ src/QDirect3D9Widget/ImGui/QDirect3D9Widget.h
@@ -13,67 +13,67 @@
 
 class QDirect3D9Widget : public QWidget
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	QDirect3D9Widget(QWidget * parent);
-	~QDirect3D9Widget();
+    QDirect3D9Widget(QWidget * parent);
+    ~QDirect3D9Widget();
 
-	void release();
-	void resetEnvironment();
+    void release();
+    void resetEnvironment();
 
 private:
-	bool init();
+    bool init();
 
-	void beginScene();
-	void endScene();
+    void beginScene();
+    void endScene();
 
-	void tick();
-	void render();
-	void uiRender();
-	
+    void tick();
+    void render();
+    void uiRender();
+
 // Qt Events
 private:
-	bool event(QEvent * event) override;
-	void showEvent(QShowEvent * event) override;
-	QPaintEngine * paintEngine() const override;
-	void paintEvent(QPaintEvent * event) override;
-	void resizeEvent(QResizeEvent * event) override;
+    bool event(QEvent * event) override;
+    void showEvent(QShowEvent * event) override;
+    QPaintEngine * paintEngine() const override;
+    void paintEvent(QPaintEvent * event) override;
+    void resizeEvent(QResizeEvent * event) override;
 
-	LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 #if QT_VERSION >= 0x050000
-	bool nativeEvent(const QByteArray & eventType, void * message, long * result) override;
+    bool nativeEvent(const QByteArray & eventType, void * message, long * result) override;
 #else
-	bool winEvent(MSG * message, long * result) override;
+    bool winEvent(MSG * message, long * result) override;
 #endif
 
 
 Q_SIGNALS:
-	void deviceInitialized(bool success);
+    void deviceInitialized(bool success);
 
-	void eventHandled();
-	void widgetResized();
+    void eventHandled();
+    void widgetResized();
 
-	void ticked();
-	void rendered();
-	void uiRendered();
+    void ticked();
+    void rendered();
+    void uiRendered();
 
 
 private Q_SLOTS:
-	void onFrame();
-	void onReset();
+    void onFrame();
+    void onReset();
 
 // Getters / Setters
 public:
-	HWND const & nativeHandle() const { return m_hWnd; }
+    HWND const & nativeHandle() const { return m_hWnd; }
 
     IDirect3DDevice9 * device() const { return m_pDevice; }
     D3DPRESENT_PARAMETERS * devicePresentParams() { return &m_PresentParams; }
     IDirect3D9 * direct3D() const { return m_pD3D; }
 
-	bool renderActive() const { return m_bRenderActive; }
-	void setRenderActive(bool active) { m_bRenderActive = active; }
+    bool renderActive() const { return m_bRenderActive; }
+    void setRenderActive(bool active) { m_bRenderActive = active; }
 
 
 private:
@@ -82,11 +82,11 @@ private:
     D3DPRESENT_PARAMETERS    m_PresentParams;
 
 
-	QTimer                    m_qTimer;
+    QTimer                    m_qTimer;
 
-	HWND                      m_hWnd;
-	bool                      m_bDeviceInitialized;
+    HWND                      m_hWnd;
+    bool                      m_bDeviceInitialized;
 
-	bool                      m_bRenderActive;
+    bool                      m_bRenderActive;
 
 };
