2e5264c75dbc445126641b213ef7e62791b545c3
Gilad Reich <giladreichgr@gmail.com>
2019-03-04 22:00:01 +0100

    Implement Direct3D11 widget including imgui widget example.

---
+++ examples/Direct3D11Widget/MainWindow.h
@@ -0,0 +1,38 @@
+/*
+ *
+ */
+
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_MainWindow.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = Q_NULLPTR);
+    ~MainWindow();
+
+    void adjustWindowSize();
+
+
+private:
+    void closeEvent(QCloseEvent * event) override;
+
+
+public Q_SLOTS:
+    bool init(bool success);
+    void tick();
+    void render();
+
+
+private:
+    Ui::MainWindowClass * ui;
+
+    bool             m_bWindowInit;
+    bool             m_bWindowClosing;
+
+
+};
