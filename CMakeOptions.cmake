f6687344de1966dd7606fa50731fadcf36f36ade
Gilad Reich <giladreichgr@gmail.com>
2020-06-07 10:39:01 +0200

    Implement cmake module to clone imgui if it doesn't exists.

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -1,5 +1,13 @@
 
 cmake_minimum_required(VERSION 3.12)
 
-option(BUILD_WITH_IMGUI "Set to OFF if you don't want to include ImGui Examples." ON)
 
+option(BUILD_WITH_IMGUI      "Set to OFF if you don't want to include ImGui Examples."       ON)
+
+option(INCLUDE_DX9_EXAMPLE   "Set to OFF if you don't want to include Direct3D9 Examples."   ON)
+
+option(INCLUDE_DX10_EXAMPLE  "Set to OFF if you don't want to include Direct3D10 Examples."  ON)
+
+option(INCLUDE_DX11_EXAMPLE  "Set to OFF if you don't want to include Direct3D11 Examples."  ON)
+
+option(INCLUDE_DX12_EXAMPLE  "Set to OFF if you don't want to include Direct3D12 Examples."  ON)
